582365de81723fd7d8b0f2653b15c4f7c796ff67
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-10-03 21:32:33 +0200

    Added routing

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <geo-location />
+    <render-less-geo-location />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import GeoLocation from "@/components/GeoLocation.vue";
+import RenderLessGeoLocation from "@/components/RenderLessGeoLocation.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    GeoLocation,
+    RenderLessGeoLocation
+  }
+};
+</script>
