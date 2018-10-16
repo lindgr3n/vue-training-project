d9100425b5fe1c7f2b5cc0d51cf7d2225703e2cd
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-10-16 21:41:32 +0200

    Not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Sorry this have not been implemented yet!</h1>
+    Pleas submit an PR at <a href="https://github.com/lindgr3n/vue-training-project" target="_blank"> vue-training-project</a> for it if you want to speed it up!
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    name() {
+      return this.data;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
